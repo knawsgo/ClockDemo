f4df267acc823c6886e0ecd379c961ff223ea200
Marcus Creese <marcuscreese94@gmail.com>
2022-02-11 08:26:04 -0600

    Commiting my JavaProjects Folder

---
+++ MyFrame.java
@@ -0,0 +1,89 @@
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+
+
+public class MyFrame extends JFrame {
+
+
+    //Calendar calendar;
+    SimpleDateFormat timeFormat;
+    SimpleDateFormat dayFormat;
+    SimpleDateFormat dateFormat;
+
+
+    JLabel timeLabel;
+    JLabel dayLabel;
+    JLabel dateLabel;
+
+    String time;
+    String day;
+    String date;
+
+
+    MyFrame() {
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setTitle("My Clock Program");
+        this.setLayout(new FlowLayout());
+        this.setSize(350, 200);
+        //this.setResizable(false);
+
+        timeFormat = new SimpleDateFormat("hh:mm:ss a");
+        dayFormat = new SimpleDateFormat("EEEE");
+        dateFormat = new SimpleDateFormat("MMMMM dd, yyyy");
+
+        timeLabel = new JLabel();
+        timeLabel.setFont(new Font("Verdana", Font.PLAIN, 50));
+        timeLabel.setForeground(new Color(0xadd8e6));
+        timeLabel.setBackground(Color.black);
+        timeLabel.setOpaque(true);
+
+        dayLabel = new JLabel();
+        dayLabel.setFont(new Font("Verdana", Font.PLAIN, 35));
+        dayLabel.setForeground(new Color(0xadd8e6));
+        dayLabel.setBackground(Color.black);
+        dayLabel.setOpaque(true);
+
+
+        dateLabel = new JLabel();
+        dateLabel.setFont(new Font("Ink Free", Font.PLAIN, 25));
+        dateLabel.setBackground(Color.black);
+        dateLabel.setOpaque(true);
+        dateLabel.setForeground(new Color(0xadd8e6));
+
+        getContentPane().setBackground(Color.black);
+
+
+        this.add(timeLabel);
+        this.add(dayLabel);
+        this.add(dateLabel);
+        this.setVisible(true);
+
+        setTime();
+    }
+
+    public void setTime() {
+        while (true) {
+            time = timeFormat.format(Calendar.getInstance().getTime());
+            timeLabel.setText(time);
+
+            day = dayFormat.format(Calendar.getInstance().getTime());
+            dayLabel.setText(day);
+
+            date = dateFormat.format(Calendar.getInstance().getTime());
+            dateLabel.setText(date);
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
---
+++ src/creese/dev/Main.java
@@ -0,0 +1,10 @@
+package creese.dev;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        new MyFrame();
+
+    }
+}
